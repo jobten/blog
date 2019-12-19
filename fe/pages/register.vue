0a29600083a38497793e7fbf927714d0cd4fe877
Will <1017665401@qq.com>
2019-12-19 18:01:39 +0800

    上传初始工程

---
+++ fe/pages/register.vue
@@ -0,0 +1,220 @@
+<template>
+  <div class="login-container">
+    <el-form ref='form' :model="form" :rule="registerRule" class="login-form">
+      <div class="title-container">
+        <img src="/logo.png" alt="">
+      </div>
+      <el-form-item prop="email">
+        <span class="svg-container">
+          <i class="el-icon-mobile">
+          </i>
+        </span>
+        <el-input
+          ref = 'email'
+          v-model='form.email'
+          placeholder = '邮箱'
+          name="email"
+          >
+        </el-input>
+      </el-form-item>
+
+
+          <!-- 邮箱验证码 -->
+      <el-form-item prop="emailcode" class="email-code">
+        <div class="send-email-btn">
+          <el-button type="primary" @click="sendCode">发送</el-button>
+        </div>
+        <span class="svg-container">
+          <i class="el-icon-user">
+          </i>
+        </span>
+
+        <el-input
+          ref = 'emailcode'
+          v-model='form.emailcode'
+          placeholder = '邮箱验证码'
+          name="emailcode"
+          >
+
+        </el-input>
+      </el-form-item>
+
+
+      <el-form-item prop="nickname">
+        <span class="svg-container">
+          <i class="el-icon-user">
+          </i>
+        </span>
+        <el-input
+          ref = 'nickname'
+          v-model='form.nickname'
+          placeholder = '昵称'
+          name="nickname"
+          >
+
+        </el-input>
+      </el-form-item>
+
+      <!-- 邮箱验证码 -->
+      <el-form-item prop="emailcode" class="email-code">
+        <div class="send-email-btn">
+          <img :src="code.captcha" @click="resetCptcha" alt="">
+        </div>
+        <span class="svg-container">
+          <i class="el-icon-user">
+          </i>
+        </span>
+
+        <el-input
+          ref = 'captcha'
+          v-model='form.captcha'
+          placeholder = '邮箱验证码'
+          name="captcha"
+          >
+
+        </el-input>
+      </el-form-item>
+
+
+    </el-form>
+
+
+  </div>
+</template>
+<script>
+export default {
+  layout:'login',
+  data() {
+    return {
+      form: {
+        email:'1017665401@qq.com',
+        nickname:'Will',
+        emailcode:'1234',
+        captcha:'abcd',
+        password:'123456',
+        repassword:'123456'
+      },
+      code:{
+        captcha:'/api/user/captcha'
+      },
+      registerRule: {
+        email: [
+          {required:true, message:'请输入邮箱'},
+          {type:'email', message:'请输入正确的邮箱'},
+        ]
+      }
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+.email-code{
+  width:340px;
+  position: relative;
+  .send-email-btn{
+    position: absolute;
+    right:-110px;
+    .el-button{
+      width:90px;
+      height:50px;
+    }
+    img{
+            width:90px;
+      height:50px;
+      cursor: pointer;
+    }
+  }
+}
+/* reset element-ui css */
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+
+    input {
+      background: transparent;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      height: 47px;
+
+    }
+  }
+
+  .el-form-item {
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    // background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+
+  .tips {
+    font-size: 14px;
+    margin-bottom: 10px;
+
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    vertical-align: middle;
+    width: 30px;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+    text-align: center;
+    margin-bottom:20px;
+    img{
+      width:200px;
+
+    }
+
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .auth{
+    margin-top:10px;
+    text-align: center;
+    img{
+      width:45px;
+      height:45px;
+      margin: 0 30px;
+    }
+  }
+
+}
+</style>
